e355f34f8a2e2a61bb020c76e6bf9c8993fae9e5
MasterYooda1 <jakealderthay@gmail.com>
2025-11-19 17:39:29 +0000

    Worked on First Test and Improved Solar System File

--- SolarSystem.py
+++ SolarSystem.py
@@ -7,7 +7,7 @@ from poliastro import constants
 from astropy.constants import G
 from spiceypy import sxform, mxvg
 
-def toStateVec(pos, vel):
+def toStateVec(pos, vel, t):
     """
     Input:
     - Takes in the Cartesian Form of Astropy Data
@@ -40,182 +40,198 @@ def toStateVec(pos, vel):
     
     return position, velocity
 
-t = Time("2025-11-17 14:25:00.0", scale="tdb") # Current Time
+def loadBodies(t):
 
-# Create the list of the Bodies in the Simulation
-bodies = []
+    # Create the list of the Bodies in the Simulation
+    bodies = []
 
-# The Sun
-# Get the Position and Velocity Vectors in Cartesian form from the JPL Ephemeris
-pos, vel = get_body_barycentric_posvel("sun", t, ephemeris="jpl")
-# Convert them to usable vectors
-position, velocity = toStateVec(pos, vel)
-# Get the mass of the sun
-mSun = (constants.GM_sun / G).value
-# Create an instance of Particle for the sun
-Sun = Particle(
-    position=np.array(position),
-    velocity=np.array(velocity),
-    acceleration=np.array([0, 0, 0]),
-    name="Sun",
-    mass=mSun 
-)
-# Add the Sun to the list of stellar bodies
-bodies.append(Sun)
+    # The Sun
+    # Get the Position and Velocity Vectors in Cartesian form from the JPL Ephemeris
+    pos, vel = get_body_barycentric_posvel("sun", t, ephemeris="jpl")
+    # Convert them to usable vectors
+    position, velocity = toStateVec(pos, vel, t)
+    # Get the mass of the sun
+    mSun = (constants.GM_sun / G).value
+    # Create an instance of Particle for the sun
+    Sun = Particle(
+        position=np.array(position),
+        velocity=np.array(velocity),
+        acceleration=np.array([0, 0, 0]),
+        name="Sun",
+        mass=mSun 
+    )
+    # Add the Sun to the list of stellar bodies
+    bodies.append(Sun)
 
-# Mercury
-pos, vel = get_body_barycentric_posvel("mercury", t, ephemeris="jpl")
-position, velocity = toStateVec(pos, vel)
-mMercury = (constants.GM_mercury / G).value
-Mercury = Particle(
-    position=np.array(position),
-    velocity=np.array(velocity),
-    acceleration=np.array([0, 0, 0]),
-    name="Mercury",
-    mass=mMercury 
-)
-bodies.append(Mercury)
+    # Mercury
+    pos, vel = get_body_barycentric_posvel("mercury", t, ephemeris="jpl")
+    position, velocity = toStateVec(pos, vel, t)
+    mMercury = (constants.GM_mercury / G).value
+    Mercury = Particle(
+        position=np.array(position),
+        velocity=np.array(velocity),
+        acceleration=np.array([0, 0, 0]),
+        name="Mercury",
+        mass=mMercury 
+    )
+    bodies.append(Mercury)
 
-# Venus
-pos, vel = get_body_barycentric_posvel("venus", t, ephemeris="jpl")
-position, velocity = toStateVec(pos, vel)
-mVenus = (constants.GM_venus / G).value
-Venus = Particle(
-    position=np.array(position),
-    velocity=np.array(velocity),
-    acceleration=np.array([0, 0, 0]),
-    name="Venus",
-    mass=mVenus 
-)
-bodies.append(Venus)
+    # Venus
+    pos, vel = get_body_barycentric_posvel("venus", t, ephemeris="jpl")
+    position, velocity = toStateVec(pos, vel, t)
+    mVenus = (constants.GM_venus / G).value
+    Venus = Particle(
+        position=np.array(position),
+        velocity=np.array(velocity),
+        acceleration=np.array([0, 0, 0]),
+        name="Venus",
+        mass=mVenus 
+    )
+    bodies.append(Venus)
 
-# Earth
-pos, vel = get_body_barycentric_posvel("earth", t, ephemeris="jpl")
-position, velocity = toStateVec(pos, vel)
-mEarth = (constants.GM_earth / G).value
-Earth = Particle(
-    position=np.array(position),
-    velocity=np.array(velocity),
-    acceleration=np.array([0, 0, 0]),
-    name="Earth",
-    mass=mEarth 
-)
-bodies.append(Earth)
+    # Earth
+    pos, vel = get_body_barycentric_posvel("earth", t, ephemeris="jpl")
+    position, velocity = toStateVec(pos, vel, t)
+    mEarth = (constants.GM_earth / G).value
+    Earth = Particle(
+        position=np.array(position),
+        velocity=np.array(velocity),
+        acceleration=np.array([0, 0, 0]),
+        name="Earth",
+        mass=mEarth 
+    )
+    bodies.append(Earth)
 
-pos, vel = get_body_barycentric_posvel("moon", t, ephemeris="jpl")
-position, velocity = toStateVec(pos, vel)
-mMoon = (constants.GM_moon / G).value
-Moon = Particle(
-    position=np.array(position),
-    velocity=np.array(velocity),
-    acceleration=np.array([0, 0, 0]),
-    name="Moon",
-    mass=mMoon 
-)
-bodies.append(Moon)
+    pos, vel = get_body_barycentric_posvel("moon", t, ephemeris="jpl")
+    position, velocity = toStateVec(pos, vel, t)
+    mMoon = (constants.GM_moon / G).value
+    Moon = Particle(
+        position=np.array(position),
+        velocity=np.array(velocity),
+        acceleration=np.array([0, 0, 0]),
+        name="Moon",
+        mass=mMoon 
+    )
+    bodies.append(Moon)
 
-# Mars
-pos, vel = get_body_barycentric_posvel("mars", t, ephemeris="jpl")
-position, velocity = toStateVec(pos, vel)
-mMars = (constants.GM_mars / G).value
-Mars = Particle(
-    position=np.array(position),
-    velocity=np.array(velocity),
-    acceleration=np.array([0, 0, 0]),
-    name="Mars",
-    mass=mMars 
-)
-bodies.append(Mars)
+    # Mars
+    pos, vel = get_body_barycentric_posvel("mars", t, ephemeris="jpl")
+    position, velocity = toStateVec(pos, vel, t)
+    mMars = (constants.GM_mars / G).value
+    Mars = Particle(
+        position=np.array(position),
+        velocity=np.array(velocity),
+        acceleration=np.array([0, 0, 0]),
+        name="Mars",
+        mass=mMars 
+    )
+    bodies.append(Mars)
 
-# Jupiter
-pos, vel = get_body_barycentric_posvel("jupiter", t, ephemeris="jpl")
-position, velocity = toStateVec(pos, vel)
-mJupiter = (constants.GM_jupiter / G).value
-Jupiter = Particle(
-    position=np.array(position),
-    velocity=np.array(velocity),
-    acceleration=np.array([0, 0, 0]),
-    name="Jupiter",
-    mass=mJupiter 
-)
-bodies.append(Jupiter)
+    # Jupiter
+    pos, vel = get_body_barycentric_posvel("jupiter", t, ephemeris="jpl")
+    position, velocity = toStateVec(pos, vel, t)
+    mJupiter = (constants.GM_jupiter / G).value
+    Jupiter = Particle(
+        position=np.array(position),
+        velocity=np.array(velocity),
+        acceleration=np.array([0, 0, 0]),
+        name="Jupiter",
+        mass=mJupiter 
+    )
+    bodies.append(Jupiter)
 
-# Saturn
-pos, vel = get_body_barycentric_posvel("saturn", t, ephemeris="jpl")
-position, velocity = toStateVec(pos, vel)
-mSaturn = (constants.GM_saturn / G).value
-Saturn = Particle(
-    position=np.array(position),
-    velocity=np.array(velocity),
-    acceleration=np.array([0, 0, 0]),
-    name="Saturn",
-    mass=mSaturn 
-)
-bodies.append(Saturn)
+    # Saturn
+    pos, vel = get_body_barycentric_posvel("saturn", t, ephemeris="jpl")
+    position, velocity = toStateVec(pos, vel, t)
+    mSaturn = (constants.GM_saturn / G).value
+    Saturn = Particle(
+        position=np.array(position),
+        velocity=np.array(velocity),
+        acceleration=np.array([0, 0, 0]),
+        name="Saturn",
+        mass=mSaturn 
+    )
+    bodies.append(Saturn)
 
-# Uranus
-pos, vel = get_body_barycentric_posvel("uranus", t, ephemeris="jpl")
-position, velocity = toStateVec(pos, vel)
-mUranus = (constants.GM_uranus / G).value
-Uranus = Particle(
-    position=np.array(position),
-    velocity=np.array(velocity),
-    acceleration=np.array([0, 0, 0]),
-    name="Uranus",
-    mass=mUranus 
-)
-bodies.append(Uranus)
+    # Uranus
+    pos, vel = get_body_barycentric_posvel("uranus", t, ephemeris="jpl")
+    position, velocity = toStateVec(pos, vel, t)
+    mUranus = (constants.GM_uranus / G).value
+    Uranus = Particle(
+        position=np.array(position),
+        velocity=np.array(velocity),
+        acceleration=np.array([0, 0, 0]),
+        name="Uranus",
+        mass=mUranus 
+    )
+    bodies.append(Uranus)
 
-# Neptune
-pos, vel = get_body_barycentric_posvel("neptune", t, ephemeris="jpl")
-position, velocity = toStateVec(pos, vel)
-mNeptune = (constants.GM_neptune / G).value
-Neptune = Particle(
-    position=np.array(position),
-    velocity=np.array(velocity),
-    acceleration=np.array([0, 0, 0]),
-    name="Neptune",
-    mass=mNeptune 
-)
-bodies.append(Neptune)
+    # Neptune
+    pos, vel = get_body_barycentric_posvel("neptune", t, ephemeris="jpl")
+    position, velocity = toStateVec(pos, vel, t)
+    mNeptune = (constants.GM_neptune / G).value
+    Neptune = Particle(
+        position=np.array(position),
+        velocity=np.array(velocity),
+        acceleration=np.array([0, 0, 0]),
+        name="Neptune",
+        mass=mNeptune 
+    )
+    bodies.append(Neptune)
 
-# Pluto
-pos, vel = get_body_barycentric_posvel("pluto", t, ephemeris="jpl")
-position, velocity = toStateVec(pos, vel)
-mPluto = (constants.GM_pluto / G).value
-Pluto = Particle(
-    position=np.array(position),
-    velocity=np.array(velocity),
-    acceleration=np.array([0, 0, 0]),
-    name="Pluto",
-    mass=mPluto 
-)
-bodies.append(Pluto)
+    # Pluto
+    pos, vel = get_body_barycentric_posvel("pluto", t, ephemeris="jpl")
+    position, velocity = toStateVec(pos, vel, t)
+    mPluto = (constants.GM_pluto / G).value
+    Pluto = Particle(
+        position=np.array(position),
+        velocity=np.array(velocity),
+        acceleration=np.array([0, 0, 0]),
+        name="Pluto",
+        mass=mPluto 
+    )
+    bodies.append(Pluto)
+    
+    return bodies
 
-stepCount = 20000 # How many steps the program will simulate
-Data = [] # Data to be written to file
+def runSim(bodies, saveInterval, stepCount=20000, interval=86400):
+    Data = [] # Data to be written to file
 
-interval = 86400  # How long the interval between calculated steps is in seconds
-saveInterval = 1 # How Frequently the Data is saved (=50 is every 50 steps)
+    interval = 50  # How long the interval between calculated steps is in seconds
+    saveInterval = 1 # How Frequently the Data is saved (=50 is every 50 steps)
 
-for step in range(stepCount):
-    # Set the Magnitude of Acceleration to 0 for each body so it can be recalculated every step
-    for body in bodies:
-        body.acceleration = np.array([0.0, 0.0, 0.0], dtype=float)
+    for step in range(stepCount):
+        # Set the Magnitude of Acceleration to 0 for each body so it can be recalculated every step
+        for body in bodies:
+            body.acceleration = np.array([0.0, 0.0, 0.0], dtype=float)
 
-    # Calculate the Acceleration for Body 2 on Body 1 and Sum to the overall acceleration vector
-    # Has a big o of O(n^2) so for a large quantity of bodies it will be inefficient.
-    for i, body1 in enumerate(bodies):
-        for j, body2 in enumerate(bodies):
-            if i != j: # If the Body is not itself 
-                body1.updateGravitationalAcceleration(body2)
+        # Calculate the Acceleration for Body 2 on Body 1 and Sum to the overall acceleration vector
+        # Has a big o of O(n^2) so for a large quantity of bodies it will be inefficient.
+        for i, body1 in enumerate(bodies):
+            for j, body2 in enumerate(bodies):
+                if i != j: # If the Body is not itself 
+                    body1.updateGravitationalAcceleration(body2)
 
-    # Loop through each body and update Positon and Velocity
-    for body in bodies:
-        body.update(interval)
+        # Loop through each body and update Positon and Velocity
+        for body in bodies:
+            body.update(interval)
 
-    # If the Step is a Multiple of the saveInterval Write to file.
-    if step % saveInterval == 0:
-        Data.append([step, *(copy.deepcopy(b) for b in bodies)]) # Writes the Step number as well as a copy of the data for each body in the system
+        # If the Step is a Multiple of the saveInterval Write to file.
+        if step % saveInterval == 0:
+            Data.append([step, *(copy.deepcopy(b) for b in bodies)]) # Writes the Step number as well as a copy of the data for each body in the system
 
-np.save("TwoBodyTest.npy", Data, allow_pickle=True) # Writes the List Data to the aforementioned file
+    np.save("TwoBodyTest.npy", Data, allow_pickle=True) # Writes the List Data to the aforementioned file
+    
+saveInterval = 1
+
+def main():
+    t = Time("2025-11-17 14:25:00.0", scale="tdb") # Current Time
+    bodies = loadBodies(t)
+    runSim(bodies, saveInterval=saveInterval)
+
+if __name__ == "__main__": # Only Runs if You're not Exporting a Function to another file
+    main()
+    
+
+    
+    
